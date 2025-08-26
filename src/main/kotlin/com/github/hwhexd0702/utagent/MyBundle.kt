5c20bf599b201b630a4b5511b84f8ce639a7f30c
GitHub Action <action@github.com>
2025-08-26 02:07:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/hwhexd0702/utagent/MyBundle.kt
+++ src/main/kotlin/com/github/hwhexd0702/utagent/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hwhexd0702.utagent
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hwhexd0702/utagent/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/hwhexd0702/utagent/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.hwhexd0702.utagent.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
